fd7c32a68920dc43260642fb2e326b679af4eb46
Lukas Ketzer <lkskbs8169782xqcfx@gmail.com>
2024-04-13 19:41:47 +0200

    added links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script>
     import Entry from "$lib/Entry.svelte";
     import folie_woche1 from "$lib/lessons/week1/test.txt";
-    const lessons = [
-        {
-            week: 1,
-            files: [
-                {
-                    url: folie_woche1,
-                    text: "Folien",
-                },
-            ],
-        },
-    ];
+    // const lessons = [
+    //     {
+    //         week: 1,
+    //         files: [
+    //             {
+    //                 url: folie_woche1,
+    //                 text: "Folien",
+    //             },
+    //         ],
+    //     },
+    // ];
+    const lessons = []
 </script>
 
 <svelte:head>
@@ -19,13 +20,40 @@
 </svelte:head>
 
 <div class="container">
-    <div style="margin-top: 5em; margin-bottom: 5em">
-        <h1 class="title is-1 is-size-3-mobile" style="text-align: center;">
-            Grundlagen: Rechenarchitektur
-        </h1>
-        <h1 class="subtitle is-3 is-size-5-mobile" style="text-align: center;">
-            Sommersemester 2024 | Lukas Ketzer
-        </h1>
+    <div class="header">
+        <div>
+            <h1 class="title is-1 is-size-3-mobile" style="text-align: center;">
+                Grundlagen: Rechenarchitektur
+            </h1>
+            <h1
+                class="subtitle is-3 is-size-5-mobile"
+                style="text-align: center;"
+            >
+                Sommersemester 2024 | Lukas Ketzer
+            </h1>
+        </div>
+
+        <div class="links">
+            <a href="https://github.com/lukasketzer" class="navbar-item">
+                <span class="icon-text">
+                    <span class="icon">
+                        <i class="bi bi-github"></i>
+                    </span>
+                    Github
+                </span>
+            </a>
+            <a href="mailto:lukas.a.ketzer@tum.de" class="navbar-item">
+                <span class="icon-text">
+                    <span class="icon">
+                        <i class="bi bi-envelope"></i>
+                    </span>
+                    lukas.a.ketzer@tum.de
+                </span>
+            </a>
+            <div>
+                <p class="navbar-item">Zulip: Lukas Ketzer</p>
+            </div>
+        </div>
     </div>
 
     <div id="entries">
@@ -82,6 +110,18 @@
         justify-content: center;
         flex-direction: column;
     }
-    .button {
+    .links {
+        display: flex;
+        justify-content: center;
+        align-self: center;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    .icon {
+        padding-bottom: 3px;
+    }
+    .header {
+        margin-top: 5em;
+        margin-bottom: 5em;
     }
 </style>
