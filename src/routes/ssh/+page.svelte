c6bc2ae3b31b5a75271999f0c5c864ebb4a7182b
Lukas Ketzer <lukas.a.ketzer@tum.de>
2024-04-22 23:00:26 +0200

    nochwas

--- src/routes/ssh/+page.svelte
+++ src/routes/ssh/+page.svelte
@@ -2,6 +2,7 @@
     import SSH from "$lib/ssh/SSH.svelte";
     import Header from "$lib/Header.svelte";
     import Navbar from "$lib/Navbar.svelte";
+    import Links from "$lib/Links.svelte";
 </script>
 
 <svelte:head>
@@ -10,7 +11,12 @@
 
 <div class="container">
     <Header name={"SSH Guide"} />
-
+    <Links
+        email={"lukas.a.ketzer@tum.de"}
+        github_link={"https://github.com/lukasketzer"}
+        zulip={"https://zulip.in.tum.de/#narrow/dm/11498-Lukas-Ketzer"}
+        zulip_name={"Lukas Ketzer"}
+    />
     <Navbar is_active={2} />
 
     <SSH />
