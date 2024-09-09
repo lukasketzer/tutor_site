24a570c634826ff83ed73091984f25ba493f1ae3
Lukas Ketzer <lkskbs8169782xqcfx@gmail.com>
2024-09-09 19:40:31 +0200

    restored original version

--- src/routes/ssh/+page.svelte
+++ src/routes/ssh/+page.svelte
@@ -1,24 +1,29 @@
 <script>
-    import Entries from "$lib/lukas/Entries.svelte";
+    import SSH from "$lib/ssh/SSH.svelte";
     import Header from "$lib/Header.svelte";
     import Navbar from "$lib/Navbar.svelte";
     import Links from "$lib/Links.svelte";
 </script>
 
 <svelte:head>
-    <title>GRA SS24</title>
+    <title>SSH Guide</title>
 </svelte:head>
 
 <div class="container">
-    <Header name={"Lukas Ketzer"} />
+    <Header name={"SSH Guide"} />
     <Links
         email={"lukas.a.ketzer@tum.de"}
         github_link={"https://github.com/lukasketzer"}
         zulip={"https://zulip.in.tum.de/#narrow/dm/11498-Lukas-Ketzer"}
         zulip_name={"Lukas Ketzer"}
     />
+    <Navbar is_active={2} />
 
-    <Navbar is_active={0} />
-
-    <Entries />
+    <SSH />
 </div>
+
+<style>
+    .container {
+        margin-top: 2rem;
+    }
+</style>
